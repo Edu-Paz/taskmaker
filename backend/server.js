9817ad19c7e159dadd15412f4a3e0baba449dd9f
Eduardo Paz <eduardopazcarv@gmail.com>
2025-04-28 21:55:15 -0300

    feat: criar rotas e controller para tarefas

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const taskRoutes = require("./routes/taskRoutes");
+
+app.use(express.json());// Para aceitar JSON
+app.use("/api", taskRoutes); // Rota base
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
